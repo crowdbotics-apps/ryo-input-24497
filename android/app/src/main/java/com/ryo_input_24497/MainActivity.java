383a18943b44d87ee1bb68648d2029d61f9a2b7f
Crowdbotics <team@crowdbotics.com>
2021-02-13 14:28:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ryo_input_24497/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ryo_input_24497;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "ryo_input_24497";
+    }
+}
